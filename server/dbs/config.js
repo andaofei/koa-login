c562f8d420313945f483dbe72db49b52b2a774e1
zhuaf <zhuaf@authine.com>
2019-02-13 09:09:50 +0800

    register&&login

---
+++ server/dbs/config.js
@@ -0,0 +1,12 @@
+// 通过设置get设置只读
+export default {
+  dbs: 'mongodb://127.0.0.1:27017/project',
+  redis: {
+    get host() {
+      return '127.0.0.1'
+    },
+    get port() {
+      return 6379
+    }
+  }
+}
